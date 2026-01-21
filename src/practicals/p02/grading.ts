6c8d15d10112579ce6b3bda80d124407a7afcc93
zone <shisanupong5415@gmail.com>
2026-01-21 14:11:25 +0700

    p02

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,27 @@
-// Write your code below
+const score = process.argv[2];
+let grade : string;
+if ((typeof score === 'number') && (score >= 0 && score <= 100)) {
+    switch(score) {
+        case score <= 49 :
+            grade = "F"
+            break
+        case score <= 59 :
+            grade = "D"
+            break
+        case score <= 69 :
+            grade = "C"
+            break
+        case score <= 79 :
+            grade = "B"
+            break
+        case score <= 100 :
+            grade = "A"
+            break
+        default :
+            console.log("Invalid input")
+            break
+    }
+}
+else {
+    console.log("Invalid input")
+}
