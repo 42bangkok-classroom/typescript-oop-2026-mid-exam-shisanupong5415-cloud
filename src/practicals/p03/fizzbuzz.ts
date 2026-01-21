7d150d2832ba9f3cde1b17f8e029e8299e396013
zone <shisanupong5415@gmail.com>
2026-01-21 15:02:52 +0700

    p03

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,20 @@
-// Write your code below
+let fb = process.argv[2];
+let fb_num = Number(fb)
+if ((isNaN(fb_num) == true) && (fb_num <= 0)) {
+}
+else {
+    for(let i = 1 ; i <= fb_num ; i++) {
+        if (((i % 3) == 0) && ((i % 5) == 0)) {
+            console.log('FizzBuzz')
+        }
+        else if ((i % 3) == 0) {
+            console.log('Fizz')
+        }
+        else if ((i % 5) == 0) {
+            console.log('Buzz')
+        }
+        else {
+            console.log(i)
+        }
+    }
+}
