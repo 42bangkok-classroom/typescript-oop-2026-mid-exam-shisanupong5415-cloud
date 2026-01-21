a9571ac88fe57a2927d1018ca5e41e423eab9d11
zone <shisanupong5415@gmail.com>
2026-01-21 15:45:35 +0700

    p04

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,13 @@
-// Write your code below
+let ali = process.argv[2];
+let ali_num = Number(ali)
+let char : string ="";
+let ary: number[] = [];
+if ((isNaN(ali_num) == true) && (ali_num <= 0)) {
+}
+else {
+    for(let i = 1 ; i <= ali_num ; i++) {
+        ary.push(i);
+        ary.sort((a,b) => b - a);
+        console.log(' '.repeat(ali_num-i)+ary.toString().replace(/,/g,""))
+    }
+}
