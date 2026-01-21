c12f5b088995c0b565bd6e6e015cb3f4e1101033
zone <shisanupong5415@gmail.com>
2026-01-21 16:26:20 +0700

    p05

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,6 +1,6 @@
 const comment = "TypeScript is a strongly typed programming language"
 let text_array = comment.split(" ");
-export function shortenComment(comment?: string){
+export function shortenComment(comment?: string) : string {
   const filter_array: string[] = text_array.filter((text_array) => text_array.length >= 5 && text_array.length <= 10 )
   return filter_array.toString().replace(/,/g," ") ?? "";
 }
