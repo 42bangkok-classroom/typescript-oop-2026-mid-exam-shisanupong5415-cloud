88a9f127d7b11c68df5e98f625612febfb26744a
zone <shisanupong5415@gmail.com>
2026-01-21 16:19:27 +0700

    p05

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -4,4 +4,3 @@ export function shortenComment(comment?: string){
   const filter_array: string[] = text_array.filter((text_array) => text_array.length >= 5 && text_array.length <= 10 )
   return filter_array.toString().replace(/,/g," ") ?? "";
 }
-shortenComment(comment)
